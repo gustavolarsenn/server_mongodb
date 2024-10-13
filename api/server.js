4ed31c4fde2166d66a3bcf15102da30da8903621
gustavolarsenn <william_gl02@hotmail.com>
2024-10-13 16:44:23 -0300

    Adicionando dados de previsão do tempo consolidado

--- api/server.js
+++ api/server.js
@@ -9,17 +9,34 @@ const uri = `mongodb+srv://${process.env.user_mongodb}:${process.env.password_mo
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+async function fetchRainData(){
+  const ID_CIDADE = '4970'
+  const TOKEN_CLIMATEMPO = '247b20282967aedbad798b36adc5e6ab'
+  try{
+    const res = await fetch(`http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/${ID_CIDADE}/days/15?token=${TOKEN_CLIMATEMPO}`)
+    
+    const data = await res.json()
 
-// require("./mongo.js")(app);
-// // Connect to MongoDB
+    const rainData = {
+      cityId: data.id,
+      cityName: data.name,
+      cityState: data.state,
+      cityCountry: data.country,
+      api: data.meteogram,
+      rainForecast: {
+        date: data.data[0].date,
+        date_br: data.data[0].date_br,
+        rainPrecipitation: data.data[0].rain.precipitation,
+        rainProbability: data.data[0].rain.probability
+      }
+    }
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html');
-// });
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
+    return rainData
+  } catch (error) {
+    console.error(error)
+    return "Erro ao buscar dados: " + error
+  }
+}
 
 MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(client => {
@@ -27,8 +44,13 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     const collection = db.collection(process.env.collection_mongodb);
 
     // Endpoint to insert data
-    app.post('/api/data', (req, res) => {
+    app.post('/api/data', async (req, res) => {
       const data = req.body;
+
+      const rainData = await fetchRainData()
+
+      data.rainData = rainData
+      
       collection.insertOne(data)
         .then(result => {
           res.status(200).send('Dados inseridos');
@@ -38,6 +60,10 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         });
     });
 
+    app.get('/rainData', (req, res) => {
+      const data = fetchRainData()
+      res.send(data)
+    });
     app.get('/', (req, res) => {
       res.sendFile(__dirname + '/index.html');
     });
@@ -47,5 +73,3 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     });
   })
   .catch(error => console.error(error));
-
-  
